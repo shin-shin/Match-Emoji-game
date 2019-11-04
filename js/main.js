7241a5b3b96175e6d466efd43fa685170ab2829e
Irene Tokareva <longhorn09@gmail.com>
2019-11-03 19:53:23 -0600

    add event listeners

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 /*----- constants -----*/ 
+
 /*----- app's state (variables) -----*/ 
+let board, clicked
 /*----- cached element references -----*/ 
 board = document.querySelector(".board");
+share = document.querySelector(".share");
+reset = document.querySelector(".reset");
+
 /*----- event listeners -----*/ 
+board.addEventListener("click", handleClick);
+share.addEventListener("click", shareClick);
+reset.addEventListener("click", resetClick);
+
 /*----- functions -----*/
 init();
+function handleClick(e){
+    
+    clicked = e.target;
+
+    if(clicked.className === "board") return;
+    console.log(clicked);
+}
+function shareClick(e){
+
+}
+function resetClick(e){
+    board.innerHTML = "";
+
+    init();
+
+}
 function build() {
     for (var i = 0; i < 44; i++) {
         board.innerHTML += "<div></div>";
